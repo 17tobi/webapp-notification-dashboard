57f5a51f18f582b2055a5df2866c91d9b3ffba61
Tobias Sturzenegger <tobias.sturzenegger@asinfotrack.ch>
2022-06-27 14:16:17 +0200

    added auto ws reconnect

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -23,25 +23,24 @@ import {IEventListEntry} from "@/models/EventModels";
 
     private events: IEventListEntry[] = [];
 
-    private get testevents(): IEventListEntry[] {
-      return [
-        {
-          title: "Test",
-          category: Departments.DEVOPS,
-          ts: "2022-01-01T00:00:00Z"
-        },
-        {
-          title: "sdtste",
-          category: Departments.SWE,
-          ts: "2022-01-01T00:00:00Z"
-        },
-      ]
-    }
-
 	private mounted(): void {
 
 		this.api( process.env.VUE_APP_API_BASE_URL + '/notifications').then((r: IEventListEntry[]) => this.events.push(...r));
+		this.connect();
+	}
 
+	private api(url: string): Promise<IEventListEntry[]> {
+		return fetch(url)
+			.then(response => {
+				if (!response.ok) {
+					throw new Error(response.statusText)
+				}
+				return response.json()
+			})
+	}
+
+	public connect(): void {
+		let self = this;
 		let connection = new WebSocket(process.env.VUE_APP_WS_BASE_URL);
 		connection.onmessage = (event) => {
 			if (event.data.match('created: ')) {
@@ -56,19 +55,20 @@ import {IEventListEntry} from "@/models/EventModels";
 				}
 			}
 		}
+		connection.onclose = (e) => {
+			setTimeout(function () {
+				self.connect();
+			}, 1000);
+		};
+
+		connection.onerror = (err) => {
+			connection.close();
+		};
 	}
 
-	private api(url: string): Promise<IEventListEntry[]> {
-		return fetch(url)
-			.then(response => {
-				if (!response.ok) {
-					throw new Error(response.statusText)
-				}
-				return response.json()
-			})
-	}
+}
 
-	}
+
 </script>
 
 <style lang="scss" scoped>
