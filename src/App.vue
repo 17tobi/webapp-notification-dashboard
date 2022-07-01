66dc7542d0d3b825d8782527354c4eb055a439e7
Pascal Lüscher <pascal.luescher@asinfotrack.ch>
2022-07-01 09:46:45 +0200

    added theme to svg

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <section class="hero has-bg-img is-fullheight">
+    <div class="bg-image">
+      <Background />
+    </diV>
     <div class="hero-head mt-5">
       <div class="container has-text-centered">
         <Clock/>
@@ -15,11 +18,12 @@
 <script lang="ts">
 	import {Component} from "vue-property-decorator";
   import Clock from "@/components/Clock.vue";
+  import Background from "@/components/Background.vue";
   import Menu from "@/components/Menu.vue";
 	import Vue from 'vue';
 
 	@Component({
-		components: {Menu, Clock}
+		components: {Menu, Clock, Background}
 	})
 	export default class App extends Vue {
 
@@ -32,9 +36,20 @@ body {
   font-family: 'Roboto';
 
 	.has-bg-img {
-		background: url('@/assets/bg/night.svg')center center; background-size:cover;
+		
 		max-height: 100vh;
 		overflow-y: hidden;
+    > .bg-image {
+      position: absolute;
+      left:0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      > background {
+        width: 100%;
+        height: 100%;
+      }
+    }
 	}
 }
 
